adca235341d9a22fdf9ba93ec3799b4f2f5bc368
shoNagai <strawberry4062@gmail.com>
2019-06-17 15:15:32 +0900

    fix scroll

--- src/ItemMeasurer.js
+++ src/ItemMeasurer.js
@@ -137,10 +137,10 @@ export default class ItemMeasurer extends Component<ItemMeasurerProps, void> {
           ? Math.ceil(node.offsetWidth)
           : Math.ceil(node.offsetHeight);
 
-      console.log(`index ${index}, oldSize ${oldSize}, newSize ${newSize}`);
+      // TODO
+      // console.log(`index ${index}, oldSize ${oldSize}, newSize ${newSize}`);
 
-      // TODO: <=
-      if (oldSize <= newSize) {
+      if (oldSize !== newSize) {
         handleNewMeasurements(index, newSize, isCommitPhase);
       }
     }
