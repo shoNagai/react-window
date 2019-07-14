8b131aa0793d7156b81bc92ae8416d1122345660
shoNagai <strawberry4062@gmail.com>
2019-07-14 20:38:29 +0900

    add log

--- src/ItemMeasurer.js
+++ src/ItemMeasurer.js
@@ -93,13 +93,6 @@ export default class ItemMeasurer extends Component<ItemMeasurerProps, void> {
     }
   }
 
-  shouldComponentUpdate(nextProps) {
-    if (nextProps.size !== this.props.size) {
-      return true;
-    }
-    return false;
-  }
-
   componentWillUnmount() {
     if (this._resizeObserver !== null) {
       this._resizeObserver.disconnect();
