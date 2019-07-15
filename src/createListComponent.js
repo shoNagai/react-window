eb5a9f23b4580cd528512613907502428f135dde
shoNagai <strawberry4062@gmail.com>
2019-07-15 20:38:23 +0900

    add log

--- src/createListComponent.js
+++ src/createListComponent.js
@@ -269,6 +269,19 @@ export default function createListComponent({
       this._unmountHook();
     }
 
+    shouldComponentUpdate(nextProps, nextState) {
+      // if (this.props.itemCount !== nextProps.itemCount) {
+      //   return true;
+      // }
+      // if (this.state.scrollOffset !== nextState.scrollOffset) {
+      //   return true;
+      // }
+      console.log('shouldComponentUpdate');
+      console.log(nextProps, this.props);
+      console.log(nextState, this.state);
+      return true;
+    }
+
     render() {
       const {
         className,
